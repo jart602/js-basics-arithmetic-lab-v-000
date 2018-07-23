b2f6fec219a65e39d0269226ae867068165eae36
Jose Arturo Esquivel Renteria <jart3450@yahoo.com>
2018-07-23 06:54:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let newID = oldID + 1000000000;
 
 let ageIsValid = Number.isInteger(currentAge);
 
-let randomNumber = Math.random;
+let randomNumber = Math.random * 20 + 1;
 
 let randomInteger = Math.floor(randomNumber);
 
